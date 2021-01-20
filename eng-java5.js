e84f8ab56b21378756f4db58363a6ab5753dc93d
hiteshkumawatpro <77722116+hiteshkumawatpro@users.noreply.github.com>
2021-01-20 14:29:00 +0530

    Add files via upload

---
+++ eng-java5.js
@@ -0,0 +1,284 @@
+var clock=setInterval(clocktiming,1000);
+function clocktiming(){
+var date=new Date(); 
+var time=date.toLocaleTimeString();
+
+document.getElementById('timeshow').innerHTML="Current Time:"+time;
+}
+
+
+function popupbox() {
+    var element = document.getElementById("tipbox");
+    if (element.style.visibility == "visible") {
+        element.style.visibility = "hidden";
+    } else {
+        element.style.visibility = "visible" ;
+    }
+}
+
+
+
+function popupdownload1(){
+    var element = document.getElementById("downpro1");
+    if (element.style.visibility == "hidden") {
+          element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+    }
+    else {
+        element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+    }
+
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/img (1).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 1')
+}
+
+
+function popupdownload2(){
+    var element = document.getElementById("downpro2");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+document.getElementById('anchor').setAttribute('href','website-assests/english 5/img (2).jpg')
+document.getElementById('anchor').setAttribute('download','joke lovers 2')
+
+}
+
+
+
+function popupdownload3(){
+    var element = document.getElementById("downpro3");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/img (3).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 3')
+}
+
+
+
+function popupdownload4(){
+    var element = document.getElementById("downpro4");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/img (4).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 4')
+}
+
+
+
+function popupdownload5(){
+    var element = document.getElementById("downpro5");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+      document.getElementById('anchor').setAttribute('href','website-assests/english 5/img (5).jpg')
+      document.getElementById('anchor').setAttribute('download','joke lovers 5')
+}
+
+
+
+
+function popupdownload6(){
+    var element = document.getElementById("downpro6");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/mid-img (1).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 6')
+}
+
+
+
+
+
+function popupdownload7(){
+    var element = document.getElementById("downpro7");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/mid-img (2).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 7')
+}
+
+
+
+
+function popupdownload8(){
+    var element = document.getElementById("downpro8");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/mid-img (3).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 8')
+}
+
+
+
+
+function popupdownload9(){
+    var element = document.getElementById("downpro9");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/mid-img (4).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 9')
+}
+
+
+
+function popupdownload10(){
+    var element = document.getElementById("downpro10");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/last (1).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 10')
+}
+
+function popupdownload11(){
+    var element = document.getElementById("downpro11");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/last (2).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 11')
+}
+
+
+
+
+
+function popupdownload12(){
+    var element = document.getElementById("downpro12");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/last (3).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 12')
+}
+
+
+
+
+
+function popupdownload13(){
+    var element = document.getElementById("downpro13");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+
+    document.getElementById('anchor').setAttribute('href','website-assests/english 5/last (4).jpg')
+    document.getElementById('anchor').setAttribute('download','joke lovers 13')
+}
+
+
+
+
+function popupdownload14(){
+    var element = document.getElementById("downpro14");
+    if (element.style.visibility == "hidden") {
+        element.setAttribute('style','visibility:visible;opacity:1; background-color: rgba(233, 150, 122, 0.622);')
+  }
+  else {
+      element.setAttribute('style','visibility:hidden; background-color: rgba(233, 150, 122, 0.622);')
+  }
+    document.getElementById('anchor').setAttribute('href',"website-assests/english 5/last (5).jpg")
+    document.getElementById('anchor').setAttribute('download','joke lovers 14')
+}
+
+
+
+
+function downloadwindow(){
+    
+    document.getElementById('downloadwindow').setAttribute('style','visibility:visible;transition-duration:1s;')
+    document.getElementById('downloadbox').setAttribute('style','visibility:visible;transition-duration:1s;')
+}
+
+
+function cl(){
+    document.getElementById('downloadwindow').setAttribute('style','visibility:hidden;transition-duration:.2s;')
+    document.getElementById('downloadbox').setAttribute('style','visibility:hidden;transition-duration:.2s;')
+    document.getElementById('readmode').setAttribute('style','visibility:hidden;transition-duration:.2s;')
+}
+
+
+function openreadmode(){
+    document.getElementById('readmode').setAttribute('style','visibility:visible;')
+}
+
+
+var imagesplay= ['website-assests/english 5/img (1).jpg','website-assests/english 5/img (2).jpg','website-assests/english 5/img (3).jpg','website-assests/english 5/img (4).jpg','website-assests/english 5/img (5).jpg','website-assests/english 5/mid-img (1).jpg','website-assests/english 5/mid-img (2).jpg','website-assests/english 5/mid-img (3).jpg','website-assests/english 5/mid-img (4).jpg','website-assests/english 5/last (1).jpg','website-assests/english 5/last (2).jpg','website-assests/english 5/last (3).jpg','website-assests/english 5/last (4).jpg','website-assests/english 5/last (5).jpg']
+var index = 0;
+
+var the_image = document.getElementById("imagesscreen");
+the_image.src = imagesplay[0];
+document.getElementById('downloadhref').href= imagesplay[0]
+
+function buttonnextplay(){
+  if(index <=12){
+  index++;
+  the_image.src = imagesplay[index];
+  document.getElementById('previousimage').setAttribute('style','opacity:1');  
+  document.getElementById('downloadhref').href= imagesplay[index];
+  document.getElementById('downloadhref').setAttribute('download','jokelovers assest')
+
+}
+
+  else{
+      document.getElementById('nextimagebutton').setAttribute('style','opacity:0.3');
+  }
+
+ 
+}
+
+
+function buttonpreviousplay(){
+    if(index >=1){
+      index--;
+    the_image.src = imagesplay[index];
+    document.getElementById('nextimagebutton').setAttribute('style','opacity:1');
+    }
+    else{
+        document.getElementById('previousimage').setAttribute('style','opacity:0.3');
+    }
+
+  }
